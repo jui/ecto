e1039a4edb194effb94f960814d3f8750fb60cfc
Jussi Uusi-Illikainen <jui@kepit.fi>
2014-10-31 13:19:22 +0200

    mysql worker fix

--- lib/ecto/adapters/mysql/worker.ex
+++ lib/ecto/adapters/mysql/worker.ex
@@ -10,15 +10,18 @@ defmodule Ecto.Adapters.Mysql.Worker do
   alias Ecto.Adapters.Mysql.Error
 
   def start(args) do
-    :gen_server.start({:global, :mysql_worker_pid}, __MODULE__, args, [])
+      GenServer.start(__MODULE__, args)
+#    :gen_server.start({:global, :mysql_worker_pid}, __MODULE__, args, [])
   end
 
   def start_link(args) do
-    :gen_server.start_link({:global, :mysql_worker_pid}, __MODULE__, args, [])
+      GenServer.start_link(__MODULE__, args)
+#    :gen_server.start_link({:global, :mysql_worker_pid}, __MODULE__, args, [])
   end
 
   def query!(_worker, sql, params, timeout \\ @timeout) do #compare
-    r = handle_query(:gen_server.call({:global, :mysql_worker_pid}, { :query, sql, params, timeout }, timeout))
+    r = handle_query(GenServer.call(worker, {:query, sql, params, opts}, opts[:timeout]))
+# r = handle_query(:gen_server.call({:global, :mysql_worker_pid}, { :query, sql, params, timeout }, timeout))
 
     r = case r do
       %Error{msg: msg, query: nil, params: nil} ->
